a847acfad9d5a897123743207eebfd9fba46b374
awsified <71472995+awsified@users.noreply.github.com>
2021-02-10 00:02:05 -0500

    added modify/delete still not working

--- assets.py
+++ assets.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField
+from wtforms.validators import InputRequired, Length, Regexp, NumberRange
+from datetime import date
+
 app = Flask(__name__)
+
+# Flask-WTF requires an enryption key - the string can be anything
+app.config['SECRET_KEY'] = 'MLXH243GssUWwKdTWS7FDhdwYF56wPj8'
 
 Bootstrap(app)
 
@@ -21,6 +29,34 @@ class Asset(db.Model):
     serial = db.Column(db.String)
     mac = db.Column(db.String)
     location = db.Column(db.String)
+
+    def __init__(self, machine, employee, tag, serial, mac, location):
+        self.machine = machine
+        self.employee = employee
+        self.tag = tag
+        self.serial = serial
+        self.mac = mac
+        self.location = location
+
+class AddRecord(FlaskForm):
+    # id used only by update/edit
+    id_field = HiddenField()
+    machine = StringField('Enter machine name', [ InputRequired(), Regexp(r'^[A-Za-z0-9\s\-\']+$', message="Invalid machine name"),
+        Length(min=12, max=12, message="Invalid machine length")
+        ])
+    employee = StringField('Enter employee name', [ InputRequired(), Regexp(r'^[A-Za-z\s\-\']+$', message="Invalid employee name")])
+    tag = StringField('IIA tag')
+    serial = StringField('HP serial tag')
+    mac = StringField('MAC Address')
+    location = StringField('Location')
+    
+    submit = SubmitField('Add/Update Record')
+
+# small form
+class DeleteForm(FlaskForm):
+    id_field = HiddenField()
+    purpose = HiddenField()
+    submit = SubmitField('Delete This Asset')
     
 
 @app.route('/')
@@ -29,8 +65,8 @@ def index():
     return render_template('index.html', machines=machines)
 
 
-@app.route('/full/<machine>')
-def full(machine):
+@app.route('/inventory/<machine>')
+def inventory(machine):
     try:
         machines = Asset.query.filter_by(machine=machine).order_by(Asset.serial).all()
         return render_template('list.html', machines=machines, machine=machine)
@@ -39,6 +75,101 @@ def full(machine):
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
+
+# add a new item to the database
+@app.route('/add_record', methods=['GET', 'POST'])
+def add_record():
+    form1 = AddRecord()
+    if form1.validate_on_submit():
+        machine = request.form['machine']
+        employee = request.form['employee']
+        tag = request.form['tag']
+        serial = request.form['serial']
+        mac = request.form['mac']
+        location = request.form['location']
+        # the data to be inserted into Sock model - the table, socks
+        record = Asset(machine, employee, tag, serial, mac, location)
+        # Flask-SQLAlchemy magic adds record to database
+        db.session.add(record)
+        db.session.commit()
+        # create a message to send to the template
+        message = f"The data for asset {machine} has been submitted."
+        return render_template('add_record.html', message=message)
+    else:
+        # show validaton errors
+        # see https://pythonprogramming.net/flash-flask-tutorial/
+        for field, errors in form1.errors.items():
+            for error in errors:
+                flash("Error in {}: {}".format(
+                    getattr(form1, field).label.text,
+                    error
+                ), 'error')
+        return render_template('add_record.html', form1=form1)
+
+# select a record to edit or delete
+@app.route('/select_record/')
+def select_record():
+    assets = Asset.query.filter(Asset.machine).order_by(Asset.machine).all()
+    return render_template('select_record.html', assets=assets)
+
+# edit or delete - come here from form in /select_record
+@app.route('/edit_or_delete', methods=['POST'])
+def edit_or_delete():
+    machine = request.form['machine']
+    choice = request.form['choice']
+    asset = Asset.query.filter(Asset.machine == machine).first()
+    # two forms in this template
+    form1 = AddRecord()
+    form2 = DeleteForm()
+    return render_template('edit_or_delete.html', asset=asset, form1=form1, form2=form2, choice=choice)
+
+# result of delete - this function deletes the record
+@app.route('/delete_result', methods=['POST'])
+def delete_result():
+    machine = request.form['id_field']
+    purpose = request.form['purpose']
+    asset = Asset.query.filter(Asset.machine == machine).first()
+    if purpose == 'delete':
+        db.session.delete(asset)
+        db.session.commit()
+        message = f"The asset {asset.machine} has been deleted from the database."
+        return render_template('result.html', message=message)
+    else:
+        # this calls an error handler
+        abort(405)
+
+# result of edit - this function updates the record
+@app.route('/edit_result', methods=['POST'])
+def edit_result():
+    machine = request.form['id_field']
+    # call up the record from the database
+    asset = Asset.query.filter(Asset.machine == machine).first()
+    # update all values
+    asset.machine = request.form['machine']
+    asset.employee = request.form['employee']
+    asset.tag = request.form['tag']
+    asset.serial = request.form['serial']
+    asset.location = request.form['location']
+
+    form1 = AddRecord()
+    if form1.validate_on_submit():
+        # update database record
+        db.session.commit()
+        # create a message to send to the template
+        message = f"The data for asset {asset.machine} has been updated."
+        return render_template('result.html', message=message)
+    else:
+        # show validaton errors
+        asset.machine = machine
+        # see https://pythonprogramming.net/flash-flask-tutorial/
+        for field, errors in form1.errors.items():
+            for error in errors:
+                flash("Error in {}: {}".format(
+                    getattr(form1, field).label.text,
+                    error
+                ), 'error')
+        return render_template('edit_or_delete.html', form1=form1, asset=asset, choice='edit')
+
 
 
 if __name__ == '__main__':
